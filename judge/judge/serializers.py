e35049a840e64c2842cb7f6f6561a55517d835a2
S.Ashvidha <as8864@srmist.edu.in>
2025-03-12 14:14:05 +0530

    Delete judge/judge/serializers.py

--- judge/judge/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Submission
-
-class SubmissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Submission
-        fields = '__all__'
